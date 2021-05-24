dbf3a643612cd252cf4daf9c438a671c19acc0af
Ahmad575-a <ahmad.alghetheth@gmail.com>
2021-05-24 23:15:01 +0200

    created Product Data

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import HP from './img/hp.png'
+import Apple from './img/Apple.jpg'
+import Lenovo from './img/lenovo.png'
+
+const data = {
+    productData:[
+        {
+            id: 1,
+            img: Apple,
+            title: 'APPLE MacBook Air (M1,2020)',
+            desc: 'MGN63D/A, Notebook mit 13,3 Zoll Display, 8 GB RAM, 256 GB SSD, 7-Core GPU, Space Grau',
+            price: 1009,
+        },
+        {
+            id: 2,
+            img: Lenovo,
+            title: 'Lenovo ThinkBook 13s',
+            desc: '13.3" Display, Intel® Core™ i5-10210U Prozessor, 8GB Arbeitsspeicher, 256GB SSD Speicher',
+            price: 799,
+        },
+        {
+            id: 3,
+            img: HP,
+            title: 'HP Pavilion Gaming 15-ec1216ng - Gaming Notebook)',
+            desc: 'AMD Ryzen™ 7 4800H - 16GB - 512GB PCIe - NVIDIA® GeForce® GTX™ 1650 Ti',
+            price: 1200,
+        }
+    ]
+}
+
+export default data 
